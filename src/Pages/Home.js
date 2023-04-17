fbf176eeeaea9aa5cde77aae3ce23b4acac87982
ShahidaTanju <shahidasharmin450@gmail.com>
2023-04-17 16:13:38 +0600

    routing

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <p className="text-5xl font-bold">Home Page</p>
+            <p>This is home</p>
         </div>
     );
 };
